58a8673294e7c18b764279b240327cd81598a4e4
CoronelSalinasFernando <coronelsalinasfernando@gmail.com>
2024-12-02 16:43:36 -0800

    terminado

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "../../models/alumno-to";
+
+export const alumnos: Alumno[] = [
+    {
+        nombre: 'Fernando',
+        apellido: 'Coronel',
+        promedio: 9.7
+    },
+    {
+        nombre: 'Carlos',
+        apellido: 'Murillo',
+        promedio: 8.0
+    },
+    {
+        nombre: 'Alejandro',
+        apellido: 'Angulo',
+        promedio: 9.5
+    }
+]
